dc3a74329bf9847273e9000dc71cb51bf649716a
francis <francisraccaglia@macbook-air-fr.local>
2016-11-13 17:43:41 +0100

    Initial Commit

---
+++ memory/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  memory
+//
+//  Created by francis on 13/11/2016.
+//  Copyright © 2016 francis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
